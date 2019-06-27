eca5faa36b32ffaee455eee2393e7dc2bea08389
Krudda <krudda@yandex.ru>
2019-06-27 19:16:11 +0300

    first commit

---
+++ 19062019.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// alert("HELLO");
+console.log('Hello!');
+
+// let answer = confirm ("Are you her?");
+
+// console.log(answer); 
+
+// let answer = prompt("Are you her?");
+
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(decr--);
+console.log(decr);
